3e1c9a3b831ecc7573d68b84ad17b834733959aa
volodymyr derkach <vlde@ciklum.com>
2014-11-14 17:44:34 -0800

    RobotOnMatrix DP

--- ci/src/main/java/com/coding/interview/dp/RobotOnMatrix.java
+++ ci/src/main/java/com/coding/interview/dp/RobotOnMatrix.java
@@ -1,4 +1,31 @@
 package com.coding.interview.dp;
 
 public class RobotOnMatrix {
+    public int minPathMam(int[][] matrix) {
+        int res = -1;
+        if (matrix != null && matrix.length > 0 && matrix[0].length > 0) {
+            int height = matrix.length;
+            int width = matrix[0].length;
+
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (i == 0 && j == 0) {
+                        // just skip
+                    } else if (i == 0) {
+                        matrix[i][j] += matrix[i][j - 1];
+                    } else if (j == 0) {
+                        matrix[i][j] += matrix[i - 1][j];
+                    } else {
+                        int top = matrix[i - 1][j];
+                        int left = matrix[i][j - 1];
+
+                        int min = Math.min(top, left);
+                        matrix[i][j] += min;
+                    }
+                }
+            }
+            res = matrix[height - 1][width - 1];
+        }
+        return res;
+    }
 }
---
+++ ci/src/test/java/com/coding/interview/dp/RobotOnMatrixTest.java
@@ -0,0 +1,70 @@
+package com.coding.interview.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RobotOnMatrixTest {
+
+    @Test
+    public void nullTest() {
+        int minPathMam = new RobotOnMatrix().minPathMam(null);
+        Assert.assertEquals(-1, minPathMam);
+    }
+
+    @Test
+    public void fullyEmptyArray() {
+        int[][] matrix = {};
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(-1, minPathMam);
+    }
+
+    @Test
+    public void innerEmptyArray() {
+        int[][] matrix = {{}};
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(-1, minPathMam);
+    }
+
+    @Test
+    public void oneCellMatrix() {
+        int[][] matrix = {{3}};
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(3, minPathMam);
+    }
+
+    @Test
+    public void rawMatrix() {
+        int[][] matrix = {{2, 3, 2, 1}};
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(8, minPathMam);
+    }
+
+    @Test
+    public void columnMatrix() {
+        int[][] matrix = {{2}, {4}, {3}, {1}};
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(10, minPathMam);
+    }
+
+    @Test
+    public void simpleMatrix() {
+        int[][] matrix = {
+                {5, 3, 1, 1},
+                {2, 1, 4, 1},
+                {1, 4, 1, 2}
+        };
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(13, minPathMam);
+    }
+
+    @Test
+    public void maxIntegerValue() {
+        int[][] matrix = {
+                {5, 3, 1, Integer.MAX_VALUE},
+                {2, 1, 4, 1},
+                {1, 4, 1, 2}
+        };
+        int minPathMam = new RobotOnMatrix().minPathMam(matrix);
+        Assert.assertEquals(13, minPathMam);
+    }
+}
